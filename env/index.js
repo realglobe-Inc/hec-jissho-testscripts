908e8311b058e2e2e1df469248e2f1d7c2a98f39
FujiHaruka <fujiharuka@realglobe.jp>
2017-02-10 16:10:26 +0900

    Update env

--- env/index.js
+++ env/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BASE_URL: process.env.BASE_URL || 'localhost:3000',
+  BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
   SOCKET_IO_PATH: '/jissho3/sugos/report/socket.io',
   CAMERA_TOKEN: 'b2c7deea-7b51-4cd7-bfc1-09e840063f64',
   CAMERA_UUID: '35f0c2f6-dc0d-459e-b5b8-c579de229697'
