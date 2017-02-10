c59f7e4dbed474cd2190368c3a813ad85e4100b4
FujiHaruka <fujiharuka@realglobe.jp>
2017-02-10 16:10:49 +0900

    Enable launch multiple browsers

--- bin/browser.js
+++ bin/browser.js
@@ -2,16 +2,20 @@
 
 process.env.DEBUG = 'hec:*'
 
-const debug = require('debug')('hec:test:browser')
+const Debug = require('debug')
 const { spawn } = require('child_process')
 const { join } = require('path')
+const { BROWSERS = 1 } = process.env
 
 process.chdir(join(__dirname, '..'))
 
-const phantom = spawn('./bin/scripts/phantom.js')
-phantom.stdout.on('data', (data) => {
-  debug(data.toString().trim())
-})
-phantom.stderr.on('data', (data) => {
-  console.error(data.toString())
-})
+for (let i = 1; i <= BROWSERS; i++) {
+  const debug = Debug(`hec:browser:${i}`)
+  const phantom = spawn('./bin/scripts/phantom.js')
+  phantom.stdout.on('data', (data) => {
+    debug(data.toString().trim())
+  })
+  phantom.stderr.on('data', (data) => {
+    debug(data.toString())
+  })
+}
