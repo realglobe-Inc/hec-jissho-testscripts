59865c7c7392d5ee6deecbe525470dbdb3bc74cb
FujiHaruka <fujiharuka@realglobe.jp>
2017-02-16 19:17:21 +0900

    extract_id

--- scripts/evaluate/helpers/extract_id.js
+++ scripts/evaluate/helpers/extract_id.js
@@ -1,45 +1,50 @@
 /**
- * ログ行 logBody を入れた時に、 head から始まっている行に反応して、 reg にマッチする部分を返す
+ * ログ行 logBody を入れた時に、 test に合格した行について、 matcher にマッチする部分を返す
  */
-const factory = ({head, reg}) => (logBody) => {
-  if (logBody.startsWith(head)) {
-    let match = logBody.match(reg)
+const factory = ({test, matcher}) => (logBody) => {
+  if (test.test(logBody)) {
+    let match = matcher.match(logBody)
     return match[1]
   } else {
     return false
   }
 }
 
+const reportPattern = /qq:reporter:([0-9#]+)/
+const photoPattern = /(\w+-\w+-\w+-\w+-\w+)/
+
+/**
+ * 各実験パターンにおいてログ行を入れたら id 情報を適切に取ってくる関数たち
+ */
 const ExtractId = {
   App: {
     report: factory({
-      head: 'hec:report-observer Observer recieve report',
-      reg: /(qq:reporter:[0-9#]+)/
+      test: /Observer\srecieve\sreport/,
+      matcher: reportPattern
     }),
     upload: factory({
-      head: 'hec:db Executing (default): INSERT INTO `photo`',
-      reg: /\/([0-9a-z-]+)\.jpg/
+      test: /INSERT\sINTO\s`photo`/,
+      matcher: photoPattern
     })
   },
   Client: {
     report: factory({
-      head: 'hec:test:reporter REPORT_FULL_ID=',
-      reg: /REPORT_FULL_ID=(qq:reporter:[0-9#]+)/
+      test: /REPORT_FULL_ID=/,
+      matcher: reportPattern
     }),
     upload: factory({
-      head: 'ec:rest:upload Photo uploaded',
-      reg: /(\w+-\w+-\w+-\w+-\w+)/
+      test: /hec:rest:upload\sPhoto\suploaded/,
+      matcher: photoPattern
     })
   },
   Browser: {
-    // TODO 実装
     report: {
-      head: '',
-      reg: /a/
+      test: /CONSOLE\sREPORT:/,
+      matcher: reportPattern
     },
     upload: {
-      head: '',
-      reg: /a/
+      test: /CONSOLE\sPHOTO:/,
+      matcher: photoPattern
     }
   }
 }
